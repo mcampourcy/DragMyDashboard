03ab9ff1840cf0775d90af5e7a5093bb17375b2b
Mireille Campourcy <mireille.campourcy@lephare.com>
2018-05-02 15:30:25 +0200

    Add js linter

---
+++ .eslintrc.js
@@ -0,0 +1,48 @@
+module.exports = {
+  "root": true,
+  "env": {
+    "browser": true,
+    "node": true,
+    "es6": true
+  },
+  "extends": [
+    "eslint:recommended",
+    "plugin:react/recommended"
+  ],
+  "parser": "babel-eslint",
+  "parserOptions": {
+    "ecmaVersion": 6,
+    "ecmaFeatures": {
+      "jsx": true
+    },
+    "sourceType": "module"
+  },
+  "plugins": [
+    "compat",
+    "react"
+  ],
+  "settings": {
+    "polyfills": [
+      "fetch"
+    ]
+  },
+  "rules": {
+    "compat/compat": 2,
+    "indent": [
+      "error",
+      2
+    ],
+    "linebreak-style": [
+      "error",
+      "unix"
+    ],
+    "quotes": [
+      "error",
+      "single"
+    ],
+    "semi": [
+      "error",
+      "always"
+    ]
+  }
+};
